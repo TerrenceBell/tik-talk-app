b8b2afca9a4817593cd36842a47079fba64e07de
TerrenceBell <terrencekbell@gmail.com>
2020-07-29 13:20:07 -0500

    adding topic window

--- src/Messageboard.js
+++ src/Messageboard.js
@@ -2,13 +2,40 @@ import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      margin: '150px',
+      margin: '50px',
       padding: theme.spacing(3, 2),
-    
     },
+    flex: { 
+        display: 'flex'
+    }, 
+    topicWindow: { 
+        width: '20%',
+        height: '300px',
+        borderRight: '1px solid grey'
+
+    },
+
+    chatWindow: { 
+        width: '70%',
+        height: '300px'
+    },
+
+    chatBox: { 
+        width: '85%'
+        
+    }, 
+
+    button: { 
+        width: '15%'
+
+    }
+
   }));
 
 export default function Messageboard() { 
@@ -18,13 +45,33 @@ export default function Messageboard() {
         <div> 
             <div className={classes.root}>
       <Paper variant="outlined" />
-      <h3>
+      <h1>
           Tik-Talk
-      </h3>
+      </h1>
 
-      <h5> 
+      <h3> 
           Topic goes here
-      </h5>
+      </h3>
+      <div className={classes.flex}>
+          <div className={classes.topicWindow}> 
+          <List> 
+          <ListItem button>
+          
+          <ListItemText primary="Drafts" />
+        </ListItem>
+            
+          </List>
+          
+          </div>
+          <div className={classes.chatWindow}> 
+          
+          </div>
+
+      </div>
+
+      <div className={classes.flex}> 
+          
+      </div>
       <Paper variant="outlined" square />
     </div>
         </div>
