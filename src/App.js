0888b8022db85805f8545c3ffb740aa01e762bf7
TerrenceBell <terrencekbell@gmail.com>
2020-07-29 11:20:26 -0500

    adding Messageboard outer part of page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Messageboard from './Messageboard'
 function App() {
   return (
     <div className="App">
-    
+     <Messageboard />
     </div>
   );
 }
--- src/Messageboard.js
+++ src/Messageboard.js
@@ -1,2 +1,32 @@
-import Reactm from 'react';
-import './App.css'; 
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      margin: '150px',
+      padding: theme.spacing(3, 2),
+    
+    },
+  }));
+
+export default function Messageboard() { 
+
+    const classes = useStyles()
+    return( 
+        <div> 
+            <div className={classes.root}>
+      <Paper variant="outlined" />
+      <h3>
+          Tik-Talk
+      </h3>
+
+      <h5> 
+          Topic goes here
+      </h5>
+      <Paper variant="outlined" square />
+    </div>
+        </div>
+    )
+}
